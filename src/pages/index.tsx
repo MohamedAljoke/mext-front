c37fe5c0dbc29b004b6bd483315189d2a44970e1
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-09 23:39:41 -0300

    tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex  justify-center align-middle">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-red-500">
+          Hello world!
+        </h1>
       </main>
     </>
   );
