6a87a6ec1f81fdbb6771175b0a6bd41af390c2ca
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-11 20:39:35 -0300

    link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,10 +10,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex  justify-center align-middle">
-        <h1 className="text-3xl font-bold underline text-red-500">
-          Hello world!
-        </h1>
+      <main className="flex  justify-center items-center h-screen">
+        <Link href="signin">
+          <h1 className="text-3xl font-bold underline text-red-500">
+            AINDA NÃO TEM NADA VAI PARA LOGIN
+          </h1>
+        </Link>
       </main>
     </>
   );
