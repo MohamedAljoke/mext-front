3426944b7f9f9796263b0947cecf2b0adabb3554
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-13 14:56:24 -0300

    feat: lectuer material section

--- src/App/Pages/Lecture/Lecture.tsx
+++ src/App/Pages/Lecture/Lecture.tsx
@@ -3,6 +3,7 @@ import { useGetLectuer } from '@/App/services/query/lectuer.query';
 import { useRouter } from 'next/router';
 import React from 'react';
 import LectuerVideo from './components/Video/LectuerVideo';
+import LectuerMaterial from './components/Material/LectuerMaterial';
 
 export default function Lecture() {
   const router = useRouter();
@@ -12,7 +13,7 @@ export default function Lecture() {
   });
   return (
     <div className="relative w-full">
-      <div className="w-full flex flex-col gap-6  justify-center items-center xl:px-28 px-16 pt-28 ">
+      <div className="w-full flex flex-col gap-6  justify-center items-center xl:px-28 px-16 py-28 ">
         {isLoading ? (
           <div className="flex items-center justify-center h-screen">
             <Spinner />
@@ -21,6 +22,7 @@ export default function Lecture() {
           <>
             <h2 className="font-bold text-xl">{lectuer?.lecture_name}</h2>
             <LectuerVideo />
+            <LectuerMaterial pdfs={lectuer?.pdfs} />
           </>
         )}
       </div>
--- src/App/Pages/Lecture/components/Material/LectuerMaterial.tsx
+++ src/App/Pages/Lecture/components/Material/LectuerMaterial.tsx
@@ -1,5 +1,28 @@
+import { PdfSchemaType } from '@/App/services/api/schemas/Pdf.Schema';
+import Link from 'next/link';
 import React from 'react';
+import { AiOutlineFilePdf } from 'react-icons/ai';
 
-export default function LectuerMaterial() {
-  return <div>LectuerMaterial</div>;
+export default function LectuerMaterial({
+  pdfs,
+}: {
+  pdfs: PdfSchemaType[] | undefined;
+}) {
+  return (
+    <div className="w-full mt-8 min-h-[350px] bg-white50 flex justify-center rounded-2xl p-5">
+      <div>
+        <h6 className="text-lg font-semibold text-center">LECTUER MATERIAL</h6>
+        {pdfs?.map((pdf) => {
+          return (
+            <Link key={pdf.id} href={`/pdf/${pdf.id}`}>
+              <div className="flex flex-row px-8 min-w-[800px] rounded-xl mt-4 py-4 bg-[#E1F5FE] shadow-defaultShadow hover:bg-[#dbf3ff] cursor-pointer">
+                <AiOutlineFilePdf className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" />
+                <span>{pdf.pdf_name}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
