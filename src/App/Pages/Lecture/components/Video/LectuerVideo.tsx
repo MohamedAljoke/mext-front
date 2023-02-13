c24d0053094ff3104e9783d6546406e850a62e24
Mohamed <mohammed.mmaljoke@gmail.com>
2023-02-13 15:05:52 -0300

    fix css

--- src/App/Pages/Lecture/components/Video/LectuerVideo.tsx
+++ src/App/Pages/Lecture/components/Video/LectuerVideo.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function LectuerVideo() {
   return (
-    <iframe
-      className="h-[604px] w-[1000px]"
-      src={`https://www.youtube.com/embed/a3iDdp43avo`}
-      title={'mext-example'}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen
-    />
+    <div className="flex items-center justify-center bg-white50 lg:min-h-[800px] min-h-[500px] w-[100%] rounded-2xl">
+      <iframe
+        className="lg:h-[700px] h-[400px] w-[90%]"
+        src={`https://www.youtube.com/embed/a3iDdp43avo`}
+        title={'mext-example'}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </div>
   );
 }
