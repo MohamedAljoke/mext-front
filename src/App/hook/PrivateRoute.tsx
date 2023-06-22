8788f34afd548c8334d7750a0401ad54775a1bb0
Mohamed <mohammed.mmaljoke@gmail.com>
2023-06-22 15:26:45 -0300

    feat: useeffect fix

--- src/App/hook/PrivateRoute.tsx
+++ src/App/hook/PrivateRoute.tsx
@@ -1,9 +1,6 @@
 import { useRouter } from 'next/router';
-import { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, } from 'react';
 import Spinner from '../components/Loading/Loader';
-import { validateAndRefreshToken } from '../services/mutation/auth.mutation';
-import { useMutation } from 'react-query';
-import { saveToken } from '../utils/constants/tokens';
 import Header from '../components/Header/Header';
 import { AuthContext } from '../context/AuthContext';
 
@@ -13,12 +10,13 @@ interface Props {
 
 const PrivateRoute: React.FC<Props> = ({ children }) => {
   const { user, isLoading } = useContext(AuthContext)
+  console.log(user)
   const router = useRouter();
   useEffect(() => {
     if (!user && !isLoading) {
       router.push('/signin')
     }
-  }, [user])
+  }, [user, isLoading])
   return isLoading ? (
     <div className="flex items-center justify-center h-screen">
       <Spinner />
